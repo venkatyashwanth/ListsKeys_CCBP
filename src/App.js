4cf29f3e83860c091d67877121f9589916fbc6ea
venkat yashwanth <mudiliyashwanth@gmail.com>
2024-01-27 19:14:22 +0530

    single user profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import UserProfile from "./components/UserProfile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const userDetails = {
+	imageUrl: "https://randomuser.me/api/portraits/men/80.jpg",
+	name: "Venkat Yashwanth",
+	role: "Software Developer"
 }
+
+const App = () => <UserProfile userDetails={userDetails}/>
 
 export default App;
